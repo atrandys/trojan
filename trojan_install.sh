3ae69e593adeb3045fd208b565af98d51d600dcb
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-11-15 17:19:43 +0800

    Update trojan_install.sh

--- trojan_install.sh
+++ trojan_install.sh
@@ -33,31 +33,17 @@ config_trojan(){
 yum -y install  wget unzip vim tcl expect expect-devel
 mkdir /usr/src/trojan
 cd /usr/src/trojan
-
+SUBJECT="/C=US/ST=Mars/L=iTranswarp/O=iTranswarp/OU=iTranswarp/CN=example.com"
 /usr/bin/expect << EOF
 spawn openssl genrsa -des3 -out private.key 1024
-expect "Enter pass phrase for private.key:" {send "atrandys\r"}
-expect "Verifying - Enter pass phrase for private.key:" {send "atrandys\r"}
-interact
-EOF
-
-SUBJECT="/C=US/ST=Mars/L=iTranswarp/O=iTranswarp/OU=iTranswarp/CN=example.com"
-
-/usr/bin/expect << EOF
+expect "Enter pass phrase for" {send "atrandys\r"}
+expect "Verifying - Enter pass phrase for" {send "atrandys\r"}
 spawn openssl req -new -subj $SUBJECT -key private.key -out private.csr
 expect "Enter pass phrase" {send "atrandys\r"}
-interact
-EOF
-
-mv private.key private.origin.key
-
-/usr/bin/expect << EOF
 spawn openssl rsa -in private.origin.key -out private.key
 expect "Enter pass phrase" {send "atrandys\r"}
-interact
+spawn openssl x509 -req -days 3650 -in private.csr -signkey private.key -out private.crt
 EOF
-
-openssl x509 -req -days 3650 -in private.csr -signkey private.key -out private.crt
 
 cat > /usr/src/trojan/server.conf <<-EOF
 {
