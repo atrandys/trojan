f88283af39dcc0b1d80e06be51f2b7896053deab
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-11-22 10:49:27 +0800

    Update trojan_install.sh

--- trojan_install.sh
+++ trojan_install.sh
@@ -12,7 +12,7 @@ fi
 
 install_docker(){
 
-	yum remove docker docker-client docker-client-latest docker-common docker-latest  docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine		
+	yum remove -y docker docker-client docker-client-latest docker-common docker-latest  docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine		
 	yum install -y yum-utils device-mapper-persistent-data lvm2
 	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 	yum makecache fast
