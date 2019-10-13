78714fe48bbe5cdd4b70488d8058e1a3080e2dfb
atrandys <37266675+atrandys@users.noreply.github.com>
2019-10-14 01:16:43 +0800

    Update trojan_centos7.sh

--- trojan_centos7.sh
+++ trojan_centos7.sh
@@ -45,25 +45,62 @@ if [ $real_addr == $local_addr ] ; then
 	green "域名解析正常，开启安装nginx并申请https证书"
 	green "=========================================="
 	rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
-    yum install -y nginx
+    	yum install -y nginx
 	systemctl enable nginx.service
 	#设置伪装站
 	rm -rf /usr/share/nginx/html/*
 	cd /usr/share/nginx/html/
 	wget https://github.com/atrandys/v2ray-ws-tls/raw/master/web.zip
-    unzip web.zip
+    	unzip web.zip
 	systemctl start nginx.service
 	#申请https证书
 	mkdir /usr/src/trojan-cert
 	curl https://get.acme.sh | sh
 	~/.acme.sh/acme.sh  --issue  -d $your_domain  --webroot /usr/share/nginx/html/
-    ~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
+    	~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
         --key-file   /usr/src/trojan-cert/private.key \
         --fullchain-file /usr/src/trojan-cert/fullchain.cer \
         --reloadcmd  "systemctl force-reload  nginx.service"
 	cd /usr/src
 	wget https://github.com/trojan-gfw/trojan/releases/download/v1.13.0/trojan-1.13.0-linux-amd64.tar.xz
 	tar xf trojan-1.*
+	#下载trojan客户端
+	wget https://github.com/atrandys/trojan/raw/master/trojan-cli.zip
+	unzip trojan-cli.zip
+	cp /usr/src/trojan-cert/fullchain.cer /usr/src/trojan-cli/fullchain.cer
+	cat > /usr/src/trojan-cli/config.json <<-EOF
+{
+    "run_type": "client",
+    "local_addr": "127.0.0.1",
+    "local_port": 1080,
+    "remote_addr": "$your_domain",
+    "remote_port": 443,
+    "password": [
+        "hi_atrandys"
+    ],
+    "log_level": 1,
+    "ssl": {
+        "verify": true,
+        "verify_hostname": true,
+        "cert": "fullchain.cer",
+        "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:RSA-AES128-GCM-SHA256:RSA-AES256-GCM-SHA384:RSA-AES128-SHA:RSA-AES256-SHA:RSA-3DES-EDE-SHA",
+        "sni": "",
+        "alpn": [
+            "h2",
+            "http/1.1"
+        ],
+        "reuse_session": true,
+        "session_ticket": false,
+        "curves": ""
+    },
+    "tcp": {
+        "no_delay": true,
+        "keep_alive": true,
+        "fast_open": false,
+        "fast_open_qlen": 20
+    }
+}
+EOF
 	rm -rf /usr/src/trojan/server.conf
 	cat > /usr/src/trojan/server.conf <<-EOF
 {
@@ -73,7 +110,7 @@ if [ $real_addr == $local_addr ] ; then
     "remote_addr": "127.0.0.1",
     "remote_port": 80,
     "password": [
-        "password1"
+        "hi_atrandys"
     ],
     "log_level": 1,
     "ssl": {
@@ -130,6 +167,11 @@ EOF
 	chmod +x /usr/lib/systemd/system/trojan.service
 	systemctl start trojan.service
 	systemctl enable trojan.service
+	green "============================================="
+	green "Trojan已安装完成，请使用以下链接下载trojan客户端"
+	green "http://${your_domain}/trojan-cli.zip"
+	green "安装完成请立即下载并存档，1个小时后将自动删除"
+	green "============================================="
 else
 	red "================================"
 	red "域名解析地址与本VPS IP地址不一致"
