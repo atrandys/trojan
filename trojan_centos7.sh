162b5efe35f5b196229887148cee09c8c582fe2c
atrandys <37266675+atrandys@users.noreply.github.com>
2019-11-03 14:46:57 +0800

    Update trojan_centos7.sh

--- trojan_centos7.sh
+++ trojan_centos7.sh
@@ -35,7 +35,7 @@ fi
 function install_trojan(){
 systemctl stop firewalld
 systemctl disable firewalld
-yum -y install bind-utils wget unzip zip curl
+yum -y install bind-utils wget unzip zip curl tar
 green "======================="
 yellow "请输入绑定到本VPS的域名"
 green "======================="
