612ae934ca38597a35f47ffa121a809b132dcba2
atrandys <37266675+atrandys@users.noreply.github.com>
2020-02-29 21:51:45 +0800

    Update trojan_mult.sh

--- trojan_mult.sh
+++ trojan_mult.sh
@@ -130,7 +130,7 @@ green "======================="
 blue "请输入绑定到本VPS的域名"
 green "======================="
 read your_domain
-real_addr=`ping -4 ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
+real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
 local_addr=`curl ipv4.icanhazip.com`
 if [ $real_addr == $local_addr ] ; then
 	green "=========================================="
