42de943dc82a4b3aa3155d380eaeb3439c666f13
atrandys <37266675+atrandys@users.noreply.github.com>
2020-03-23 16:17:22 +0800

    Update trojan_mult.sh

--- trojan_mult.sh
+++ trojan_mult.sh
@@ -250,6 +250,8 @@ if [ -n "$Port443" ]; then
 fi
 CHECK=$(grep SELINUX= /etc/selinux/config | grep -v "#")
 if [ "$CHECK" != "SELINUX=disabled" ]; then
+    green "检测到SELinux开启状态，添加放行80/443端口规则"
+    yum install -y policycoreutils-python >/dev/null 2>&1
     semanage port -a -t http_port_t -p tcp 80
     semanage port -a -t http_port_t -p tcp 443
 fi
