3273bf9defc15084cea37cc69f21b6e67302c205
atrandys <37266675+atrandys@users.noreply.github.com>
2020-04-03 17:50:02 +0800

    Update trojan_wp.sh

--- trojan_wp.sh
+++ trojan_wp.sh
@@ -242,7 +242,7 @@ fi
 CHECK=$(grep SELINUX= /etc/selinux/config | grep -v "#")
 if [ "$CHECK" != "SELINUX=disabled" ]; then
     green "检测到SELinux开启状态，添加放行80/443端口规则"
-    yum install -y policycoreutils-python >/dev/null 2>&1
+    $systemPackage install -y policycoreutils-python >/dev/null 2>&1
     semanage port -m -t http_port_t -p tcp 80
     semanage port -m -t http_port_t -p tcp 443
 fi
