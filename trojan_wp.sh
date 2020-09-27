f22dc906fb8515c1019b5c4af15fb34953109664
atrandys <37266675+atrandys@users.noreply.github.com>
2020-09-27 17:09:03 +0800

    Update trojan_wp.sh

--- trojan_wp.sh
+++ trojan_wp.sh
@@ -75,7 +75,7 @@ function install_wordpress(){
     green "2.安装PHP7.4"
     green "============"
     sleep 1
-    yum -y install php74 php74-php-gd  php74-php-pdo php74-php-mbstring php74-php-cli php74-php-fpm php74-php-mysqlnd php74-php-xml
+    yum -y install php74 php74-php-gd php74-php-opcache php74-php-pdo php74-php-mbstring php74-php-cli php74-php-fpm php74-php-mysqlnd php74-php-xml
     service php74-php-fpm start
     chkconfig php74-php-fpm on
     if [ `yum list installed | grep php74 | wc -l` -ne 0 ]; then
